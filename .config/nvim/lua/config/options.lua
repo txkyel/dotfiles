cc1ea839bf315eb0ce929e0a945608217dc85a2b
Kyle <kylexiao20@gmail.com>
2024-05-19 12:09:55 -0400

    Add WIP nvim configs

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,20 @@
+local opt = vim.opt
+
+-- Spacing
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+
+-- Appearance
+opt.nu = true
+opt.relativenumber = true
+
+-- Search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+-- opt.hlsearch = false
+
