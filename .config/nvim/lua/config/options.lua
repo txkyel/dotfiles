dcf2c37aae8083aa7ccd6bcd2926797e2026395b
Kyle <kylexiao20@gmail.com>
2024-05-19 22:05:06 -0400

    Add nvim configs, Add nvim-tree and which-key plugins

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -9,12 +9,32 @@ opt.smartindent = true
 opt.wrap = false
 
 -- Appearance
-opt.nu = true
+opt.number = true
 opt.relativenumber = true
+opt.termguicolors = true
+opt.colorcolumn = "100"
+opt.signcolumn = "yes"
+opt.scrolloff = 10
+opt.completeopt = "menuone,noinsert,noselect"
 
 -- Search
 opt.incsearch = true
 opt.ignorecase = true
 opt.smartcase = true
 -- opt.hlsearch = false
+
+-- Behaviour
+opt.hidden = true
+opt.errorbells = false
+opt.swapfile = false
+opt.backup = false
+opt.undodir = vim.fn.stdpath("cache") .. "/vim/undodir"
+opt.undofile = true
+opt.backspace = "indent,eol,start"
+opt.splitright = true
+opt.splitbelow = true
+opt.autochdir = false
+opt.mouse:append("a")
+opt.clipboard:append("unnamedplus")
+opt.encoding = "UTF-8"
 
---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,10 @@
+return {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    opts = {
+        filters = {
+            dotfiles = true,
+        },
+    },
+}
+
