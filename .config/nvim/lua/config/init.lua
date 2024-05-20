697f75df619361cb9c5f7b4981b9d73091e099e5
Kyle <kylexiao20@gmail.com>
2024-05-20 11:56:04 -0400

    Fix nvim plugin configs

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -19,7 +19,7 @@ local opts = {
         lazy = true,
     },
     install = {
-        colorscheme = { "gruvbox" }
+        colorscheme = { "gruvbox" },
     },
     rtp = {
         disabled_plugins = {
--- .config/nvim/lua/plugins/vimbegood.lua
+++ .config/nvim/lua/plugins/vimbegood.lua
@@ -1,4 +1,5 @@
 return {
     "theprimeagen/vim-be-good",
-    lazy = false
+    lazy = false,
+    priority = 0, -- Load last
 }
