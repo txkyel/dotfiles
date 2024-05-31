30f2f91f766c5c19817f55d3a4261ec85ae5be20
Kyle <kylexiao20@gmail.com>
2024-05-30 22:59:21 -0400

    Add qtile group keybinds
    - Add keybinds to move between groups
    - Remove keybinds for focusing/moving windows left/right

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -37,17 +37,13 @@ keys = [
     ### WINDOW MANAGEMENT
     Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
     # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key(["mod1"], "tab", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move up/down in current stack.
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # TODO: Remove? Keep?
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
@@ -58,6 +54,16 @@ keys = [
     # Fullscreen
     Key([mod], "f", lazy.window.toggle_fullscreen(), desc="Toggle fullscreen"),
     Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating"),
+
+    ### SCREENS/GROUPS
+    # Groups
+    # Switch between groups
+    Key([mod], "h", lazy.screen.prev_group(), desc="Switch to prev group"),
+    Key([mod], "l", lazy.screen.next_group(), desc="Switch to next group"),
+    # TODO: Add function to move windows to next group
+    # Key([mod, "shift"], "h", lazy.screen.prev_group(skip_empty=True), desc="Switch to prev group"),
+    # Key([mod, "shift"], "l", lazy.screen.next_group(skip_empty=True), desc="Switch to next group"),
+    Key([mod], "space", lazy.screen.next_group(skip_empty=True), desc="Switch to next non-empty group"),
     # Screens
     Key([mod], "comma", lazy.prev_screen(), desc="Move focus to previous screen"),
     Key([mod], "period", lazy.next_screen(), desc="Move focus to next screen"),
@@ -68,18 +74,8 @@ keys = [
     Key([mod], "p", lazy.spawn("rofipower"), desc="Shutdown menu"),
     # terminal
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "space",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    # Toggle between different layouts as defined below
+
+      # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
@@ -116,7 +112,7 @@ keys = [
 
 
 ### GROUPS
-groups = [Group(i) for i in "123456789"]
+groups = [Group(i) for i in "123456"]
 
 for i in groups:
     keys.extend(
@@ -265,7 +261,6 @@ widgets = [
     widget.GroupBox(
         margin=6,
         disable_drag=True,
-        visible_groups=["1", "2", "3", "4", "5"],
         highlight_method="line",
         # Group label color
         active=colors["fg"],
