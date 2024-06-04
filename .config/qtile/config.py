445e8e4b5f518c548b98274913948d60920744c5
Kyle <kylexiao20@gmail.com>
2024-06-04 09:38:37 -0400

    Update window move short cut

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -130,17 +130,20 @@ for i in groups:
                 lazy.group[i.name].toscreen(),
                 desc="Switch to group {}".format(i.name),
             ),
-            # mod1 + shift + group number = switch to & move focused window to group
+            # mod1 + control + group number = move focused window to group
             Key(
-                [mod, "shift"],
+                [mod, "control"],
                 i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                lazy.window.togroup(i.name),
+                desc="move focused window to group {}".format(i.name)
             ),
-            # Or, use below if you prefer not to switch to that group.
-            # # mod1 + shift + group number = move focused window to group
-            # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            #     desc="move focused window to group {}".format(i.name)),
+            # # mod1 + control + group number = switch to & move focused window to group
+            # Key(
+            #     [mod, "control"],
+            #     i.name,
+            #     lazy.window.togroup(i.name, switch_group=True),
+            #     desc="Switch to & move focused window to group {}".format(i.name),
+            # ),
         ]
     )
 
